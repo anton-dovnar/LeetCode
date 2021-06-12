2aa8ef706a1f9e9f83416c01a9b47761723ddbc5
Anton Dovnar <fode4cun@gmail.com>
2021-06-13 02:17:16 +0300

    Build an Array With Stack Operations
    Issue #1441

---
+++ Stack/1441.py
@@ -0,0 +1,23 @@
+"""
+Build an Array With Stack Operations
+
+Runtime: 32 ms, faster than 72.57% of Python3 online submissions
+for Build an Array With Stack Operations.
+Memory Usage: 14.3 MB, less than 48.84% of Python3 online submissions
+for Build an Array With Stack Operations.
+"""
+
+
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        stack = []
+        pointer = 0
+        for number in range(1, n + 1):
+            if pointer > len(target) - 1:
+                break
+            stack.append("Push")
+            if number != target[pointer]:
+                stack.append("Pop")
+                pointer -= 1
+            pointer += 1
+        return stack
