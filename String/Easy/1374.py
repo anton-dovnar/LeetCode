513b5e410cab6ed2d1c48e722cd692416d1662e1
Anton Dovnar <fode4cun@gmail.com>
2021-06-16 02:00:36 +0300

    Generate a String With Characters That Have Odd Counts

---
+++ String/Easy/1374.py
@@ -0,0 +1,11 @@
+"""
+Generate a String With Characters That Have Odd Counts
+"""
+
+
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        if n % 2:
+            return "a" * n
+        else:
+            return "a" * (n - 1) + "b"
