941c8c7fa4f112514629d650c9410519f9df7e67
Anton Dovnar <fode4cun@gmail.com>
2021-06-19 23:23:26 +0300

    Number of Different Integers in a String

---
+++ String/Easy/1805.py
@@ -0,0 +1,9 @@
+"""
+Number of different Integers in a String
+"""
+import re
+
+
+class Solution:
+    def numDifferentIntegers(self, word: str) -> int:
+        return len(set(map(int, re.findall(r'\d+', word)))) 
