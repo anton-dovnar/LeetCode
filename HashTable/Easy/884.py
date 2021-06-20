1103896ccd0192677e6698e63e603764fb565a0c
Anton Dovnar <fode4cun@gmail.com>
2021-06-20 21:48:19 +0300

    Uncommon Words from Two Sentences

--- HashTable/Easy/884.py
+++ HashTable/Easy/884.py
@@ -5,4 +5,6 @@ Uncommon Words from Two Sentences
 
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        return set(s1.split()) ^ set(s2.split())
+        s1 = s1.split()
+        s2 = s2.split()
+        return [word for word in set(s1) ^ set(s2) if s1.count(word) + s2.count(word) == 1]
