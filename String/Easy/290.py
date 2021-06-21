95169e16513501e5dccf98666ad8f3967420df6d
Anton Dovnar <fode4cun@gmail.com>
2021-06-22 00:27:48 +0300

    Word Pattern

--- String/Easy/290.py
+++ String/Easy/290.py
@@ -3,19 +3,19 @@ Word Pattern
 """
 
 
-class solution:
-    def wordpattern(self, pattern: str, s: str) -> bool:
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split()
         if len(pattern) != len(s):
-            return false
+            return False
 
         hash_map = dict()
         for letter, word in zip(pattern, s):
             if letter in hash_map:
                 if hash_map[letter] != word:
-                    return false
+                    return False
             else:
                 if word in hash_map.values():
-                    return false
+                    return False
                 hash_map[letter] = word
-        return true
+        return True
